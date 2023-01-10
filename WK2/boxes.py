93b94e3a308aa8b8bc22d7f89f3a64338dddfb76
ferreiracamilo <camiloferreira2009@hotmail.com>
2023-01-09 22:55:40 -0300

    initial commit for week 2 assignment

---
+++ WK2/boxes.py
@@ -0,0 +1,6 @@
+manufactured_items_qty = int(input("Indicate how many items were manufactured"))
+capacity_box_qty = int(input("Indicate how many items fit in a box"))
+#the number of manufactured items
+#the number of items that the user will pack per box
+
+child_meal_price = float(input("What is the price of a child's meal? "))
