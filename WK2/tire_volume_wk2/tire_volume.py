6bbb1e274de3919e9c4d07b35ecd91818cfd9838
ferreiracamilo <camiloferreira2009@hotmail.com>
2023-01-10 19:45:14 -0300

    Remove unneeded var
    pai var created to assign math pi value is remove

--- WK2/tire_volume_wk2/tire_volume.py
+++ WK2/tire_volume_wk2/tire_volume.py
@@ -16,10 +16,9 @@ def buildPath(file_name):
         final_path = dir_path + "\\" + file_name
 #Function end
 
-pai = math.pi
 w = float(input("Enter the width of the tire in mm (ex 205): "))
 a = float(input("Enter the aspect ratio of the tire (ex 60): "))
 d = float(input("Enter the diameter of the wheel in inches (ex 15): "))
 
-v = pai * w**2 * a * (w * a + 2540 * d) / 10000000000
+v = math.pi * w**2 * a * (w * a + 2540 * d) / 10000000000
 print(f"Volume is {v:.2f}")
