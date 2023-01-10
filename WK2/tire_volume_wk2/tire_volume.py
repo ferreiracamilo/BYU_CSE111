e4c6de25120b62ed44788ae5e8c056fdc12ccd08
ferreiracamilo <camiloferreira2009@hotmail.com>
2023-01-10 19:44:21 -0300

    Build path function addition
    This is intended to cover the change from Windows to Mac OS without major issues

--- WK2/tire_volume_wk2/tire_volume.py
+++ WK2/tire_volume_wk2/tire_volume.py
@@ -1,4 +1,20 @@
 import math
+import os
+
+#Function start
+def buildPath(file_name):
+    #I am utilizing a github repo to save my assignment and I jump from time to time from Windows to Mac OS. The path will not be exactly the same, that is why I built this function
+    dir_path = os.path.dirname(os.path.realpath(__file__)) #retrieve root folder for py file
+    final_path = ""
+
+    #Review if how to proceed for next node based on previous slash or backslash presence
+    if dir_path.find("/") != -1:
+        final_path = dir_path + "/" + file_name
+    elif dir_path.find("//") != -1:
+        final_path = dir_path + "//" + file_name
+    elif dir_path.find("\\") != -1:
+        final_path = dir_path + "\\" + file_name
+#Function end
 
 pai = math.pi
 w = float(input("Enter the width of the tire in mm (ex 205): "))
