e2c1f14854ce3255e213e1981bcd2b698fb32fb9
ferreiracamilo <camiloferreira2009@hotmail.com>
2023-01-14 21:41:54 -0300

    Create test func to demosntrate python doc similar to java doc

---
+++ WK3/test_func.py
@@ -0,0 +1,10 @@
+def function_name(param1, param2, paramN):
+    """JAVA DOC EQUAL
+    @param param1 It is expected a number
+    @param param2 it is expected a string
+    @param paramN it is expected a boolean
+    @return NO RETURN FOR THIS ONE
+    """
+    print("a")
+
+function_name
