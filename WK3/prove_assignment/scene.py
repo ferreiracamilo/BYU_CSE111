c4e6e6250aabc36cf0a02844f4c38f403376e33d
ferreiracamilo <camiloferreira2009@hotmail.com>
2023-01-15 11:30:32 -0300

    Create prove assignment file with initial

---
+++ WK3/prove_assignment/scene.py
@@ -0,0 +1,34 @@
+# Import the functions from the Draw 2-D library
+# so that they can be used in this program.
+from draw2d import \
+    start_drawing, draw_line, draw_oval, draw_arc, \
+    draw_rectangle, draw_polygon, draw_text, finish_drawing
+
+
+def main():
+    # Width and height of the scene in pixels
+    scene_width = 800
+    scene_height = 500
+
+    # Call the start_drawing function in the draw2d.py
+    # library which will open a window and create a canvas.
+    canvas = start_drawing("Scene", scene_width, scene_height)
+
+    # Call your drawing functions such
+    # as draw_sky and draw_ground here.
+
+
+
+    # Call the finish_drawing function
+    # in the draw2d.py library.
+    finish_drawing(canvas)
+
+
+# Define your functions such as
+# draw_sky and draw_ground here.
+
+
+
+# Call the main function so that
+# this program will start executing.
+main()
