08417fe17e9aa9ff95d8750b9aa78a8ca63921f8
ferreiracamilo <camiloferreira2009@hotmail.com>
2023-01-15 00:29:46 -0300

    Copy template to work on it

---
+++ WK3/fitness.py
@@ -0,0 +1,79 @@
+# Import datetime so that it can be
+# used to compute a person's age.
+from datetime import datetime
+
+
+def main():
+    # Get the user's gender, birthdate, height, and weight.
+
+    # Call the compute_age, kg_from_lb, cm_from_in,
+    # body_mass_index, and basal_metabolic_rate functions
+    # as needed.
+
+    # Print the results for the user to see.
+    pass
+
+
+def compute_age(birth_str):
+    """Compute and return a person's age in years.
+    Parameter birth_str: a person's birthdate stored
+        as a string in this format: YYYY-MM-DD
+    Return: a person's age in years.
+    """
+    # Convert a person's birthdate from a string
+    # to a date object.
+    birthdate = datetime.strptime(birth_str, "%Y-%m-%d")
+    today = datetime.now()
+
+    # Compute the difference between today and the
+    # person's birthdate in years.
+    years = today.year - birthdate.year
+
+    # If necessary, subtract one from the difference.
+    if birthdate.month > today.month or \
+        (birthdate.month == today.month and \
+            birthdate.day > today.day):
+        years -= 1
+
+    return years
+
+
+def kg_from_lb(pounds):
+    """Convert a mass in pounds to kilograms.
+    Parameter pounds: a mass in U.S. pounds.
+    Return: the mass in kilograms.
+    """
+    return
+
+
+def cm_from_in(inches):
+    """Convert a length in inches to centimeters.
+    Parameter inches: a length in inches.
+    Return: the length in centimeters.
+    """
+    return
+
+
+def body_mass_index(weight, height):
+    """Compute and return a person's body mass index.
+    Parameters
+        weight: a person's weight in kilograms.
+        height: a person's height in centimeters.
+    Return: a person's body mass index.
+    """
+    return
+
+
+def basal_metabolic_rate(gender, weight, height, age):
+    """Compute and return a person's basal metabolic rate.
+    Parameters
+        weight: a person's weight in kilograms.
+        height: a person's height in centimeters.
+        age: a person's age in years.
+    Return: a person's basal metabolic rate in kcals per day.
+    """
+    return
+
+
+# Call the main function so that
+# this program will start executing.
