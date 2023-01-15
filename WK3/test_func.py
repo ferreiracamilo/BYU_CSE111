69442b0ed73584cf9be538f5368e2f62afcd4998
ferreiracamilo <camiloferreira2009@hotmail.com>
2023-01-15 00:27:56 -0300

    update vars

--- WK3/test_func.py
+++ WK3/test_func.py
@@ -7,5 +7,7 @@ def function_name(param1, param2, paramN):
     """
     print("a")
 
-paramx1 = "a", paramx2 = "a", paramxN = "a"
+paramx1 = "a"
+paramx2 = "a"
+paramxN = "a"
 function_name(paramx1, paramx2, paramxN)
