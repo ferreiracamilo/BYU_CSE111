d27c3aab7c50ebe1fcf97ccfa4b86692605fb012
ferreiracamilo <camiloferreira2009@hotmail.com>
2023-02-04 16:33:36 -0300

    test_get_noun refactoring

--- WK5/prove_assignment/test_sentences.py
+++ WK5/prove_assignment/test_sentences.py
@@ -39,6 +39,38 @@ def test_get_determiner():
 
 def test_get_noun():
     print("a")
+    # 1. Test the single determiners.
+
+    single_nouns = ["bird", "boy", "car", "cat", "child", "dog", "girl", "man", "rabbit", "woman"]
+
+    # This loop will repeat the statements inside it 4 times.
+    # If a loop's counting variable is not used inside the
+    # body of the loop, many programmers will use underscore
+    # (_) as the variable name for the counting variable.
+    for i in range(4):
+
+        # Call the get_determiner function which
+        # should return a single determiner.
+        word = get_noun(1)
+
+        # Verify that the word returned from get_determiner
+        # is one of the words in the single_determiners list.
+        assert word in single_nouns
+
+    # 2. Test the plural determiners.
+
+    plural_nouns = ["birds", "boys", "cars", "cats", "children", "dogs", "girls", "men", "rabbits", "women"]
+
+    # This loop will repeat the statements inside it 4 times.
+    for i in range(4):
+
+        # Call the get_determiner function which
+        # should return a plural determiner.
+        word = get_noun(2)
+
+        # Verify that the word returned from get_determiner
+        # is one of the words in the plural_determiners list.
+        assert word in plural_nouns
 
 def test_get_verb():
     print("a")
