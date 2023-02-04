1628462798fe77214f392d79967c1c86ee7686a9
ferreiracamilo <camiloferreira2009@hotmail.com>
2023-02-04 16:39:10 -0300

    refactor test_get_verb

--- WK5/prove_assignment/test_sentences.py
+++ WK5/prove_assignment/test_sentences.py
@@ -38,7 +38,6 @@ def test_get_determiner():
         assert word in plural_determiners
 
 def test_get_noun():
-    print("a")
     # 1. Test the single determiners.
 
     single_nouns = ["bird", "boy", "car", "cat", "child", "dog", "girl", "man", "rabbit", "woman"]
@@ -73,7 +72,64 @@ def test_get_noun():
         assert word in plural_nouns
 
 def test_get_verb():
-    print("a")
+    # 1. Test the single determiners.
+
+    past_verbs = ["drank", "ate", "grew", "laughed", "thought", "ran", "slept", "talked", "walked", "wrote"]
+
+    # This loop will repeat the statements inside it 4 times.
+    # If a loop's counting variable is not used inside the
+    # body of the loop, many programmers will use underscore
+    # (_) as the variable name for the counting variable.
+    for i in range(4):
+
+        # Call the get_determiner function which
+        # should return a single determiner.
+        word = get_verb(i, "past")
+
+        # Verify that the word returned from get_determiner
+        # is one of the words in the single_determiners list.
+        assert word in past_verbs
+
+    # 2. Test the plural determiners.
+
+    future_verbs = ["will drink", "will eat", "will grow", "will laugh", "will think", "will run", "will sleep", "will talk", "will walk", "will write"]
+
+    # This loop will repeat the statements inside it 4 times.
+    for i in range(4):
+
+        # Call the get_determiner function which
+        # should return a plural determiner.
+        word = get_verb(i, "future")
+
+        # Verify that the word returned from get_determiner
+        # is one of the words in the plural_determiners list.
+        assert word in future_verbs
+    
+    present_single_verbs = ["drinks", "eats", "grows", "laughs", "thinks", "runs", "sleeps", "talks", "walks", "writes"]
+
+    # This loop will repeat the statements inside it 4 times.
+    for i in range(4):
+
+        # Call the get_determiner function which
+        # should return a plural determiner.
+        word = get_verb(1, "present")
+
+        # Verify that the word returned from get_determiner
+        # is one of the words in the plural_determiners list.
+        assert word in present_single_verbs
+    
+    present_multi_verbs = ["drink", "eat", "grow", "laugh", "think", "run", "sleep", "talk", "walk", "write"]
+
+    # This loop will repeat the statements inside it 4 times.
+    for i in range(4):
+
+        # Call the get_determiner function which
+        # should return a plural determiner.
+        word = get_verb(2, "present")
+
+        # Verify that the word returned from get_determiner
+        # is one of the words in the plural_determiners list.
+        assert word in present_multi_verbs
 
 # Call the main function that is part of pytest so that the
 # computer will execute the test functions in this file.
