60c56427df9705a99998c31a7dadaa80bbfa6f09
ferreiracamilo <camiloferreira2009@hotmail.com>
2023-02-08 12:05:14 -0300

    Update function comment

--- WK6/teamwork/esteem.py
+++ WK6/teamwork/esteem.py
@@ -1,4 +1,14 @@
-def calculate_question_score (reply,question_type):
+def calculate_question_score (answer,question_type):
+    """Return a value to question answered by user by its reply and question type (positive,negative)
+
+    Parameter
+        answer: a string.
+            It is expected to be either 'D', 'd', 'a' or 'A'
+        question_type: a string
+            It is expected to be either 'positive' or 'negative'
+    
+    Return: an integer value based on scale given in problem statement section.
+    """
     question_score = -1
     positive_score_dictionary = {
                 "D": 0,
@@ -15,12 +25,20 @@ def calculate_question_score (reply,question_type):
                 }
     
     if question_type == "positive":
-        question_score = positive_score_dictionary[reply]
+        question_score = positive_score_dictionary[answer]
     else:
-        question_score = negative_score_dictionary[reply]
+        question_score = negative_score_dictionary[answer]
     return question_score
 
 def self_esteem_result(total_points):
+    """Return a message to display based on total score obtained by user's answers
+
+    Parameter
+        reply: int.
+            A quantity previously calculated based on user's asnwers
+    
+    Return: a string to display user's self steem statement
+    """
     message = ""
     if total_points < 15:
         message = "A score below 15 may indicate problematic low self-esteem."
@@ -29,6 +47,8 @@ def self_esteem_result(total_points):
     return message
 
 def main():
+    total_score = 0
+
     question_list = {
                 "I feel that I am a person of worth, at least on an equal plane with others": "positive",
                 "I feel that I have a number of good qualities.": "positive",
@@ -41,8 +61,6 @@ def main():
                 "I certainly feel useless at times.": "negative",
                 "At times I think I am no good at all.": "negative"
                 }
-    i = 1
-    total_score = 0
 
     print("This program is an implementation of the Rosenberg")
     print("Self-Esteem Scale. This program will show you ten")
@@ -55,6 +73,7 @@ def main():
     print("a means you agree with the statement.")
     print("A means you strongly agree with the statement.\n")
 
+    i = 1
     for question in question_list:
         print(f"{i}. {question}")
         one_reply = input("Enter D, d, a, or A: ")
