0dc28d16d093d73b78d36f877c56c608dbfb3ed5
ferreiracamilo <camiloferreira2009@hotmail.com>
2023-02-09 23:22:10 -0300

    checkpoint and initial practise wk7

---
+++ WK7/startpoint/array_attempt.py
@@ -0,0 +1,18 @@
+from array import array
+scores_list = []
+scores_list.append(95)
+scores_list.append(96)
+print(scores_list)
+print(scores_list[1])
+print()
+scores = array('d')
+scores.append(97)
+scores.append(98)
+print(scores)
+print(scores[1])
+
+print()
+person = {'first':"Christopher"}
+person["last"] = "Harrison"
+print(person)
+print(person["first"])
