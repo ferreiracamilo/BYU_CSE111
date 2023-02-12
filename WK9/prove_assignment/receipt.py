4e890dada4897601dc98e399ea68f3b75a115047
ferreiracamilo <camiloferreira2009@hotmail.com>
2023-02-12 20:02:09 -0300

    update wk9

--- WK9/prove_assignment/receipt.py
+++ WK9/prove_assignment/receipt.py
@@ -3,8 +3,6 @@ import csv
 from os import path
 
 def buildPath(file_name):
-    #file_name_path = path.join(path.dirname(__file__), file_name)
-    parentDir = path.pardir
     file_name_path = path.join(path.dirname(__file__), file_name)
     return file_name_path
 
@@ -19,10 +17,17 @@ def read_dictionary(filename, key_column_index):
     Return: a compound dictionary that contains
         the contents of the CSV file.
     """
+    outcome_dictionary = {}
     with open(buildPath(filename), "rt") as input_file:
         reader = csv.reader(input_file)
         next(reader)
         for row_list in reader:
-            print(row_list[0])
+            qty_columns = len(row_list)
+            data_value = []
+            for i in range(qty_columns):
+                if i != key_column_index:
+                    data_value.append(row_list[i])
+            outcome_dictionary[row_list[key_column_index]] = data_value
+    return outcome_dictionary
 
-read_dictionary("products.csv",1)
+print(read_dictionary("products.csv",1))
